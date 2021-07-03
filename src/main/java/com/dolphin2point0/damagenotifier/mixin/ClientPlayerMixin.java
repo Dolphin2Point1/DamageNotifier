15c2920f3a225830460eb9c8b5ece3af84ba9c75
Dolphin2Point1 <finb7729@gmail.com>
2021-07-02 20:49:35 -0500

    fix #1

--- src/main/java/com/dolphin2point0/damagenotifier/mixin/ClientPlayerMixin.java
+++ src/main/java/com/dolphin2point0/damagenotifier/mixin/ClientPlayerMixin.java
@@ -1,5 +1,6 @@
 package com.dolphin2point0.damagenotifier.mixin;
 
+import com.dolphin2point0.damagenotifier.DamageNotifier;
 import com.dolphin2point0.damagenotifier.mixin.accessor.MinecraftClientAccessor;
 import com.dolphin2point0.damagenotifier.mixin.accessor.WindowAccessor;
 import net.fabricmc.api.EnvType;
@@ -19,8 +20,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ClientPlayerMixin {
     @Inject(method = "damage", at = @At("HEAD"))
     public void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        Window window = ((MinecraftClientAccessor) client).getWindow();
-        GLFW.glfwRequestWindowAttention(((WindowAccessor) (Object) window).getHandle());
+        if(DamageNotifier.instance.windowState) {
+            Window window = ((MinecraftClientAccessor) MinecraftClient.getInstance()).getWindow();
+            GLFW.glfwRequestWindowAttention(((WindowAccessor) (Object) window).getHandle());
+        }
     }
 }
